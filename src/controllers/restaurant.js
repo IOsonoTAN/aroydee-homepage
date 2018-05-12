d51aa083ff3cd84d992aac7fe70e436947ae69d3
Krissada Boontrigratn <krissada@appsynth.net>
2018-05-12 20:15:14 +0700

    add meta tags

--- src/controllers/restaurant.js
+++ src/controllers/restaurant.js
@@ -14,8 +14,11 @@ module.exports.getDetail = async (ctx) => {
       }
     } = await axios.get(`https://api.aroydee-channel.com/v1/restaurants/${restaurantId}`)
 
+    const currentUrl = ctx.request.href
+
     await ctx.render('restaurant/detail', {
-      restaurant
+      restaurant,
+      currentUrl
     })
   } catch (e) {
     ctx.throw(404, e.message)
